c18f5a2f2e6e8a3109229ef9c9f2b2b46d8382f6
James Orr <jwillorr@gmail.com>
2025-08-19 23:39:54 -0500

    stop generating graphics on build. Add custom palette data for sprite

---
+++ source/block_sprite.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(block_sprite)
+
+//======================================================================
+//
+//	block_sprite, 16x16@4, 
+//	+ palette 256 entries, not compressed
+//	+ 4 tiles not compressed
+//	Total size: 512 + 128 = 640
+//
+//	Time-stamp: 2025-08-19, 22:32:47
+//	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_BLOCK_SPRITE_H
+#define GRIT_BLOCK_SPRITE_H
+
+#define block_spriteTilesLen 128
+extern const unsigned int block_spriteTiles[32];
+
+#define block_spritePalLen 512
+extern const unsigned short block_spritePal[256];
+
+#endif // GRIT_BLOCK_SPRITE_H
+
+//}}BLOCK(block_sprite)
